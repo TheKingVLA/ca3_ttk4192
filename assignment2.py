e06096a1bd807c3b1c5aa798843c3627220898d2
TheKingVLA <115652310+TheKingVLA@users.noreply.github.com>
2026-03-15 22:43:23 +0100

    Add solves for task 2

---
+++ assignment2.py
@@ -0,0 +1,83 @@
+from matplotlib import pyplot as plt
+from gridWorld import gridWorld
+import numpy as np
+
+def show_action_value_function(env, Q):
+    pos = {"U": (-0.15, -0.3), "D": (-0.15, 0.4), "L": (-0.45, 0.1), "R": (0.05, 0.1)}
+    fig = env.render(show_state = False, show_reward = False)            
+    for k in env.states():
+        s = env.legal_states[k]
+        for i, a in enumerate(env.actions(k)):
+            fig.axes[0].annotate("{0:.2f}".format(Q[k, i]), (s[1] + pos[a][0], s[0] + pos[a][1]), size = 40/env.board_mask.shape[0], color = "r" if Q[k, i] == max(Q[k, :]) else "k")
+    plt.show()
+    
+def show_policy(env, Q):
+    fig = env.render(show_state = False, show_reward = False)
+    action_map = {"U": "↑", "D": "↓", "L": "←", "R": "→"}
+    for k in env.states():
+        s = k if isinstance(k, tuple) else env.legal_states[k]
+        if not env.terminal(s):
+            fig.axes[0].annotate(action_map[env.actions(s)[np.argmax(Q[k, :])]], (s[1] - 0.1, s[0] + 0.1), size = 100/env.board_mask.shape[0])
+    plt.show()
+
+
+####################  Q-Learning ####################
+def Q_Learning(env, gamma, Q, alpha, epsilon):
+    # Reset environment
+    s, r, done = env.reset()
+
+    """
+    YOUR CODE HERE:
+    Implement Q-Learning
+    
+    Input arguments:
+        - env     Is the environment
+        - gamma   Is the discount rate
+        - Q       Is the Q table
+        - alpha   Is the learning rate
+        - epsilon Is the probability of choosing greedy action
+    
+    Some useful functions of the grid world environment
+        - s_next, r, done = env.step(a)  Take action a and observe the next state, reward and environment termination
+        - actions = env.actions()        List available actions in current state (is empty if state is terminal)
+    """
+
+    while (not done):
+        raise Exception("Q-learning not implemented")
+
+
+    return Q
+
+
+
+if __name__ == "__main__":
+    """
+    Note that this code requires the numpy and matplotlib packages.
+    """
+
+    # Import the environment from file
+    filename = "gridworlds/tiny.json"
+    env = gridWorld(filename)
+
+    # Render image
+    fig = env.render(show_state = True)
+    plt.show()
+
+
+    """
+    (Run Q-Learning)
+    
+    Below is the code for running Q-Learning, feel free to change the code, and tweek the parameters.
+    """
+    gamma = 1.0     # Discount rate
+    alpha = 0.1     # Learning rate
+    epsilon = 0.9   # Probability of taking greedy action
+    episodes = 5000 # Number of episodes
+
+    Q = np.zeros([len(env.states()), 4])
+    for i in range(episodes):
+        Q_Learning(env, gamma, Q, alpha, epsilon)
+
+    # Render Q-values and policy 
+    show_action_value_function(env, Q)
+    show_policy(env, Q)
